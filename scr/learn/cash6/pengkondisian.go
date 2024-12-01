4d4c11f5a5a36ba4c5bc43ddc6e06c16ca4b6da6
24jamterlewati <lookat.lookat.this@gmail.com>
2024-12-02 06:38:39 +0800

    origin  commit

---
+++ scr/learn/cash6/pengkondisian.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Print("masukkan dua angka dan pisahkan dengan spasi(ex: 10 20): ")
+	fmt.Scanf("%d %d", a, b)
+
+	if b == 1 {
+		fmt.Println(a + a)
+	} else if b == 2 {
+		fmt.Println(a * a)
+	} else if b == 3 {
+		fmt.Println("error tidak dikenali")
+	} else {
+		fmt.Println("keluar Program")
+	}
+}
