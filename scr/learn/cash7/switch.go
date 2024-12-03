83c05d137e1883b16bbff2b96197fc75561c0a79
24jamterlewati <lookat.lookat.this@gmail.com>
2024-12-03 12:10:58 +0800

    origin commit

--- scr/learn/cash7/switch.go
+++ scr/learn/cash7/switch.go
@@ -7,14 +7,31 @@ func main() {
 	fmt.Print("Masukkan dua angka pisahkan dengan spasi:")
 	fmt.Scanf("%d %d", &a, &b)
 
+	// switch tanpa expresion
 	switch {
 	case b == 1:
 		fmt.Println(a + a)
 	case b == 2:
 		fmt.Println(a * a)
 	case b == 3:
-		fmt.Println("error")
+		fmt.Println("error kode salah")
 	default:
+		fmt.Println("Keluar Program")
+	}
 
+	// switch dengan expresion
+	switch b {
+	case 1:
+		fmt.Println(a + a)
+	case 2:
+		fmt.Println(a * a)
+	case 3:
+		fmt.Println("error kode salah")
+	default:
+		fmt.Println("Keluar Program")
 	}
 }
+
+// catatan pada switch expresion:
+// inputan dgn tipe data int menggunakan case 1:
+// inputan dgn tipe data char atau string menggunakan case "1":
